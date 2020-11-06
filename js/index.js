f19fac7f33757a62d85d4a91cfddf8c816f4d168
Brenek Harrison <brenekharrison@gmail.com>
2020-11-06 16:49:21 -0700

    Functional

--- js/index.js
+++ js/index.js
@@ -1,3 +1,118 @@
+var playerScore = 0;
+var computerScore = 0;
+
+let setupComplete = false;
+
+let rockButton = null;
+let paperButton = null;
+let scissorsButton = null;
+
+let compRock = null;
+let compPaper = null;
+let compScissors = null;
+
+let playAgainButton = null;
+
+let hidingButtons = null;
+
+function setup() {
+	// Sets up global variables and event listeners
+
+	// Player buttons
+	rockButton = document.getElementById("rock");
+	paperButton = document.getElementById("paper");
+	scissorsButton = document.getElementById("scissors");
+
+	rockButton.addEventListener("click", () => { playGame("rock"); });
+	paperButton.addEventListener("click", () => { playGame("paper"); });
+	scissorsButton.addEventListener("click", () => { playGame("scissors"); });
+
+	// Computer buttons
+	compRock = document.getElementById("comp-result-rock");
+	compPaper = document.getElementById("comp-result-paper");
+	compScissors = document.getElementById("comp-result-scissors");
+
+	playAgainButton = document.getElementById("play-again-button");
+	playAgainButton.addEventListener("click", reset);
+
+	hidingButtons = [rockButton, paperButton, scissorsButton, compRock, compPaper, compScissors];
+
+	setupComplete = true;
+}
+
+function playGame(playerSelection) {
+	if (!setupComplete) setup();
+	let computerSelection = computerPlay();
+
+	hideButton(playerSelection, true);
+	hideButton(computerSelection, false);
+
+	document.getElementById("computer-result").style.visibility = "visible";
+	document.getElementById("status-text").textContent = "vs";
+
+	playAgainButton.style.visibility = "visible";
+
+	// Determine winner
+	let playerWins = doesPlayerWin(playerSelection, computerSelection);
+	if (playerWins === null) return;
+
+	if (playerWins) {
+		playerScore++;
+	} else {
+		computerScore++;
+	}
+	renderScores(playerScore, computerScore);
+}
+
+function reset() {
+	if (!setupComplete) setup();
+	document.getElementById("computer-result").style.visibility = "hidden";
+	document.getElementById("status-text").textContent = "Select one:";
+
+	hidingButtons.forEach((b) => {
+		b.style.visibility = "inherit";
+	});
+}
+
+function hideButton(buttonName="rock", playerButtons=true) {
+	// Hides all buttons except for buttonName. playerButtons is a boolean describing whether to operate on the player or computer's buttons
+	let rock;
+	let paper;
+	let scissors;
+
+	if (playerButtons) {
+		rock = rockButton;
+		paper = paperButton;
+		scissors = scissorsButton;
+	} else {
+		rock = compRock;
+		paper = compPaper;
+		scissors = compScissors;
+	}
+
+	switch (buttonName) {
+		case "rock":
+			paper.style.visibility = "hidden";
+			scissors.style.visibility = "hidden";
+			break;
+		case "paper":
+			rock.style.visibility = "hidden";
+			scissors.style.visibility = "hidden";
+			break;
+		case "scissors":
+			rock.style.visibility = "hidden";
+			paper.style.visibility = "hidden";
+			break;
+		default:
+			throw "Unknown buttonName passed to hideButton. Must be either 'rock', 'paper', or 'scissors'.";
+	}
+}
+
+function renderScores(pScore, cScore) {
+	document.getElementById("player-score").textContent = `Your Score: ${pScore}`;
+	document.getElementById("comp-score").textContent = `Opponent Score: ${cScore}`;
+}
+
 function game() {
 	// Plays a 5 round game of Rock, Paper, Scissors
 	let playerScore = 0;
@@ -10,13 +125,13 @@ function game() {
 			console.log(`Computer played: ${computerSelection}. You win.`);
 			playerScore++;
 		} else if (result === null) {
-			console.log(`Computer played: ${computerSelection}. No one wins.`)
+			console.log(`Computer played: ${computerSelection}. No one wins.`);
 		} else {
 			console.log(`Computer played: ${computerSelection}. You lose.`);
 			computerScore++;
 		}
 	}
-	console.log(`You won ${playerScore} times.\nComputer won ${computerScore} times.`)
+	console.log(`You won ${playerScore} times.\nComputer won ${computerScore} times.`);
 }
 
 function doesPlayerWin(playerSelection, computerSelection) {
